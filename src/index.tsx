213390a991eba2f3ec244e94004ec7d6f27c8277
Gabriel Queiroz <gabrielqueirozzn@gmail.com>
2020-06-27 14:03:00 -0300

    Commit inicial

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const a = '';
+
+const App: React.FC = () => (
+  <View>
+    <Text>hello world.</Text>
+  </View>
+);
+
+export default App;
